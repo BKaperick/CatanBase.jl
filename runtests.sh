9908caf6b1e34d5bea36c0de2eb0a2b58a4d740e
Bryan Kaperick <bryankaperick@gmail.com>
2025-03-18 00:33:14 +0100

    Update children

--- runtests.sh
+++ runtests.sh
@@ -2,4 +2,7 @@ cd CatanEngine.jl/test
 julia --project=.. ./runtests.jl
 cd ../..
 julia --project=CatanLearning.jl -e 'include("CatanLearning.jl/src/CatanLearning.jl"); Catan.run(["CatanEngine.jl/data/config.txt", "./CatanEngine.jl/data/sample.csv", "./CatanEngine.jl/save.txt"])'
+# TODO
+#cd CatanLearning.jl/test
+#julia --project=.. ./runtests.jl
 
