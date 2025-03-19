e0257c3719de54eec3790769ba0ef5fb04da28f7
Bryan Kaperick <bryankaperick@gmail.com>
2025-03-19 13:22:56 +0100

    Child commits, and make runtests.sh more comprehensive - run sample game from readme

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,9 @@
 cd CatanEngine.jl/test
 julia --project=.. ./runtests.jl
-cd ../..
+cd ..
+julia --project src/Catan.jl data/config.txt data/sample.csv save.txt
+cd ..
 julia --project=CatanLearning.jl -e 'include("CatanLearning.jl/src/CatanLearning.jl"); Catan.run(["CatanEngine.jl/data/config.txt", "./CatanEngine.jl/data/sample.csv", "./CatanEngine.jl/save.txt"])'
-# TODO
-#cd CatanLearning.jl/test
-#julia --project=.. ./runtests.jl
+cd CatanLearning.jl/test
+julia --project=.. ./runtests.jl
 
