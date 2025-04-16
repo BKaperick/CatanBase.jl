732403180142c427823a60b4a0b136ed29e15a9f
Bryan Kaperick <bryankaperick@gmail.com>
2025-04-16 21:42:43 +0200

    Fix run script for parallel feature gen with new RunScripts entrypoint

--- RunScripts/run.jl
+++ RunScripts/run.jl
@@ -2,7 +2,35 @@ using Catan
 using CatanLearning
 using Profile
 
-function run(config_file)
+function retrain_models(config_file)
+    (configs, player_configs, logger) = Catan.parse_configs(config_file)
+    #CatanLearning.train_and_serialize_model(player_configs["FEATURES"], player_configs["MODEL"])
+    CatanLearning.train_and_serialize_model(player_configs["PUBLIC_FEATURES"], player_configs["PUBLIC_MODEL"])
+end
+
+function run(args::Tuple)
+    if length(args) == 1
+        run(args[1])
+    else
+        run(args[1], args[2])
+    end
+end
+
+"""
+    `run(config_file::String, file_suffix)`
+
+A bit of a hack.  For parallel runs of fature generation, to stop locking issues, we pass a 
+random number to CLI via bash script so each worker is writing to a different file.
+"""
+function run(config_file::String, file_suffix)
+    (configs, player_configs, logger) = Catan.parse_configs(config_file)
+    configs["PlayerSettings"]["FEATURES"] = replace(configs["PlayerSettings"]["FEATURES"], ".csv" => "_$file_suffix.csv")
+    configs["PlayerSettings"]["PUBLIC_FEATURES"] = replace(configs["PlayerSettings"]["PUBLIC_FEATURES"], ".csv" => "_$file_suffix.csv")
+    player_schemas = Catan.read_player_constructors_from_config(player_configs)
+    CatanLearning.run(player_schemas, configs)
+end
+
+function run(config_file::String)
     (configs, player_configs, logger) = Catan.parse_configs(config_file)
     player_schemas = Catan.read_player_constructors_from_config(player_configs)
     CatanLearning.run(player_schemas, configs)
@@ -19,5 +47,5 @@ function profile_run(config_file)
 end
 
 if ~isinteractive() && length(ARGS) > 0
-    run(ARGS[1])
+    run(ARGS...)
 end
