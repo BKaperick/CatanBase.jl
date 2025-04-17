93f52d73349720b4fdecbe86b986064ac1776491
Bryan Kaperick <bryankaperick@gmail.com>
2025-04-17 18:13:59 +0200

    Re-organize configs, move to a bash script runscript system, pairing one-off configs with julia command

---
+++ RunScripts/retrain_models.sh
@@ -0,0 +1,1 @@
+julia -e "include(\"run.jl\"); retrain_models(\"configs/retrain_models.toml\")"
--- RunScripts/run_one.sh
+++ RunScripts/run_one.sh
@@ -1,3 +1,3 @@
 #julia --project -e "using CatanLearning; CatanLearning.run(CatanLearning.Catan.DefaultRobotPlayer)" $((RANDOM % 1000))
 #julia --project ./src/CatanLearning.jl "$((RANDOM % 10000))"
-julia --project run.jl feature_generation.toml "$((RANDOM % 10000))"
+julia --project run.jl configs/feature_generation.toml "$((RANDOM % 10000))"
