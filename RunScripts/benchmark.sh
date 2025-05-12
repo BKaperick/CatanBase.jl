06a72a98d204ddb3f5cc2de18d74b1d25eb56988
Bryan Kaperick <bryankaperick@gmail.com>
2025-05-12 17:52:31 +0200

    Benchmark run

--- RunScripts/benchmark.sh
+++ RunScripts/benchmark.sh
@@ -1,2 +1,2 @@
 julia -e "include(\"run.jl\"); benchmark_run(\"configs/benchmark_run.toml\")" --threads 1
-julia -e "include(\"run.jl\"); benchmark_run(\"configs/benchmark_run.toml\")" --threads 4
+#julia -e "include(\"run.jl\"); benchmark_run(\"configs/benchmark_run.toml\")" --threads 4
