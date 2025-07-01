4535af03a496f4b701bbffc397f4267e0423a0f9
Bryan Kaperick <bryankaperick@gmail.com>
2025-07-01 19:32:26 +0200

    Tests with mutations (need to specify CatanLearning as the project)

--- RunScripts/all_hybrid.sh
+++ RunScripts/all_hybrid.sh
@@ -1,1 +1,1 @@
-julia -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid.toml\")"
+julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid.toml\")"
--- RunScripts/one_hybrid.sh
+++ RunScripts/one_hybrid.sh
@@ -1,1 +1,1 @@
-julia -e "include(\"run.jl\"); run_state_space_tournament(\"configs/one_hybrid.toml\")"
+julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/one_hybrid.toml\")"
