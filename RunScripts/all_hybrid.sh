6a04904e925fff42b3a784c12995e09d2575fca3
Bryan Kaperick <bryankaperick@gmail.com>
2025-07-09 16:25:12 +0200

    First actual RL data gen

--- RunScripts/all_hybrid.sh
+++ RunScripts/all_hybrid.sh
@@ -1,3 +1,10 @@
 # First, we clear data files
-rm 
+rm ./data/state_values.csv
+rm ./models/linear_model.csv
+rm ./models/linear_public_model.csv
+julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid_explore.toml\")"
+
+# Remove weights so we retrain them based on the latest data
+rm ./models/linear_model.csv
+rm ./models/linear_public_model.csv
 julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid.toml\")"
