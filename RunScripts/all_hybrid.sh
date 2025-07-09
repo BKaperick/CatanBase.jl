b1109bd0273f2e3bed3bbebc568b17c675c6e4cd
Bryan Kaperick <bryankaperick@gmail.com>
2025-07-09 15:47:56 +0200

    Setup RL explore + optimize test

--- RunScripts/all_hybrid.sh
+++ RunScripts/all_hybrid.sh
@@ -1,2 +1,3 @@
-#julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid.toml\")"
-julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid_one_game.toml\")"
+# First, we clear data files
+rm 
+julia --project=../CatanLearning.jl -e "include(\"run.jl\"); run_state_space_tournament(\"configs/all_hybrid.toml\")"
