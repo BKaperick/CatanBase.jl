2fb72e2a0ff901bb56f7f0fff0ee18e43508cf3c
Bryan Kaperick <bryankaperick@gmail.com>
2025-08-05 08:26:45 +0200

    Benchmark, testing empath again

--- RunScripts/release_benchmark_catan.sh
+++ RunScripts/release_benchmark_catan.sh
@@ -2,21 +2,29 @@ rm benchmark_catan_release.txt
 cd ../CatanEngine.jl
 git checkout v0.1.0
 cd ../RunScripts
-julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v0.1.0\")"
+julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/default_benchmark_run.toml\", \"v0.1.0\")"
 cd ../CatanEngine.jl
 git checkout v0.2.0
 cd ../RunScripts
-julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v.0.2.0\")"
+julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/default_benchmark_run.toml\", \"v.0.2.0\")"
 cd ../CatanEngine.jl
 git checkout v0.3.0
 cd ../RunScripts
-julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v.0.3.0\")"
+julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/default_benchmark_run.toml\", \"v.0.3.0\")"
+cd ../CatanEngine.jl
+git checkout v0.4.0
+cd ../RunScripts
+julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/default_benchmark_run.toml\", \"v.0.4.0\")"
+cd ../CatanEngine.jl
+git checkout v0.5.0
+cd ../RunScripts
+julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/default_benchmark_run.toml\", \"v.0.5.0\")"
 cd ../CatanEngine.jl
 git checkout master
 cd ../RunScripts
-julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v.0.4.0\")"
+julia -e "include(\"run_without_learning.jl\"); benchmark_one_game(\"configs/default_benchmark_run.toml\", \"v.1.0.0\")"
 
 cp run_without_learning.jl ../CatanEngine.jl/benchmarks/
-cp configs/benchmark_run.toml ../CatanEngine.jl/benchmarks/
+cp configs/default_benchmark_run.toml ../CatanEngine.jl/benchmarks/
 cp release_benchmark_catan.sh ../CatanEngine.jl/benchmarks/release_benchmark.sh
 cp benchmark_catan_release.txt ../CatanEngine.jl/benchmarks/
